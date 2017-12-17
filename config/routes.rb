2d1ae0e59710dfff52172170c84167510d850157
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-12-17 18:14:04 +0900

    ルーティングの規定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'sites#index'
 
-  resources :sites, only: [:show]
+  resources :sites, only: [:show] do
+    resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
 
 end
