22488ebd42f916107020bffde9af8a4195b9f191
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-12-19 13:07:41 +0900

    postのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get 'post/new'
+
+  get 'post/create'
+
+  get 'post/edit'
+
+  get 'post/update'
+
+  get 'post/destroy'
+
   root 'sites#index'
 
   resources :sites, only: [:show] do
     resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :posts, only: [:new, :create, :edit, :update, :destroy]
   end
 
 end
