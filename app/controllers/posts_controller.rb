ff1545216a80b00461b90daa0f4e1c0dfd3d022f
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-12-20 11:35:09 +0900

    ディレクトリ名を変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,14 @@
 class PostsController < ApplicationController
+  before_action :set_site
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.site = @site
+    @post.save
   end
 
   def edit
@@ -12,5 +18,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def set_site
+    @site = Site.find(params[:site_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
