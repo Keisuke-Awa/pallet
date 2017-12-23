82329687d8a08ca1b7f16a8d853e6ba92d333d52
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-12-23 12:30:38 +0900

    データベースの適用

--- db/schema.rb
+++ db/schema.rb
@@ -23,19 +23,43 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "updated_at",                null: false
   end
 
-  add_index "categories", ["site_id"], name: "index_categories_on_site_id", using: :btree
+  create_table "items", force: :cascade do |t|
+    t.integer  "post_id",           limit: 4
+    t.integer  "item_content_id",   limit: 4
+    t.string   "item_content_type", limit: 255
+    t.integer  "order",             limit: 4,   null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "items", ["post_id"], name: "fk_posts_items", using: :btree
+
+  create_table "list_items", force: :cascade do |t|
+    t.integer  "list_id",    limit: 4,   null: false
+    t.string   "content",    limit: 255
+    t.integer  "order",      limit: 4,   null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "list_items", ["list_id"], name: "index_list_items_on_list_id", using: :btree
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255, null: false
-    t.text     "text",        limit: 255, null: false
     t.integer  "site_id",     limit: 4,   null: false
     t.integer  "category_id", limit: 4,   null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
 
-  add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
-  add_index "posts", ["site_id"], name: "index_posts_on_site_id", using: :btree
+  add_index "posts", ["category_id"], name: "fk_categories_posts", using: :btree
+  add_index "posts", ["site_id"], name: "fk_sites_posts", using: :btree
 
   create_table "sites", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
@@ -44,4 +68,14 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "updated_at",             null: false
   end
 
+  create_table "texts", force: :cascade do |t|
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_foreign_key "items", "posts", name: "fk_posts_items"
+  add_foreign_key "list_items", "lists", name: "fk_lists_list_items"
+  add_foreign_key "posts", "categories", name: "fk_categories_posts"
+  add_foreign_key "posts", "sites", name: "fk_sites_posts"
 end
